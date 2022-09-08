70ce47830cb1c05cedc9912be9843342f6fe38bd
goku727 <josetemc2001@gmail.com>
2022-09-08 21:01:40 +0200

    Better terminal and logging

---
+++ src/main/java/dev/josemc/pixucord/terminal/ConsoleCommandCompleter.java
@@ -0,0 +1,65 @@
+package dev.josemc.pixucord.terminal;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.CommandManager;
+import net.minestom.server.command.ConsoleSender;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.CommandSyntax;
+import net.minestom.server.command.builder.arguments.Argument;
+import net.minestom.server.command.builder.parser.ArgumentQueryResult;
+import net.minestom.server.command.builder.parser.CommandParser;
+import net.minestom.server.command.builder.parser.CommandQueryResult;
+import net.minestom.server.utils.StringUtils;
+import org.jline.reader.Candidate;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.ParsedLine;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ConsoleCommandCompleter implements Completer {
+    private final CommandManager commandManager = MinecraftServer.getCommandManager();
+    private final ConsoleSender sender = commandManager.getConsoleSender();
+
+    @Override
+    public void complete(LineReader reader, ParsedLine parsedLine, List<Candidate> candidates) {
+        final String commandString = parsedLine.line();
+        final String[] args = commandString.split(StringUtils.SPACE, -1);
+
+        if (args.length == 1) {
+            final String cmdName = args[args.length - 1];
+            candidates.addAll(autoCompleteCommand(commandManager.getDispatcher().getCommands(), cmdName));
+            return;
+        }
+
+        final CommandQueryResult commandQueryResult = CommandParser.findCommand(MinecraftServer.getCommandManager().getDispatcher(), commandString);
+        if (commandQueryResult == null) {
+            return;
+        }
+
+        final ArgumentQueryResult queryResult = CommandParser.findEligibleArgument(commandQueryResult.command(),
+                commandQueryResult.args(), commandString, commandString.endsWith(StringUtils.SPACE), false,
+                CommandSyntax::hasSuggestion, Argument::hasSuggestion);
+        if (queryResult == null) {
+            final String cmdName = args[args.length - 1];
+            candidates.addAll(autoCompleteCommand(commandQueryResult.command().getSubcommands(), cmdName));
+        }
+    }
+
+    private List<Candidate> autoCompleteCommand(Collection<Command> commands, String cmdName) {
+        return commands.stream()
+                .flatMap(cmd -> {
+                    if (cmd.getAliases().length > 0) {
+                        return Arrays.stream(cmd.getAliases()); // shorter cmd versions
+                    }
+                    return Stream.of(cmd.getName());
+                })
+                .filter(cmd -> cmd.contains(cmdName))
+                .map(Candidate::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/dev/josemc/pixucord/terminal/TerminalConsole.java
@@ -0,0 +1,40 @@
+package dev.josemc.pixucord.terminal;
+
+import net.minecrell.terminalconsole.SimpleTerminalConsole;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.CommandManager;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+
+public class TerminalConsole extends SimpleTerminalConsole {
+    private final CommandManager commandManager = MinecraftServer.getCommandManager();
+
+    @Override
+    protected LineReader buildReader(LineReaderBuilder builder) {
+        return super.buildReader(builder
+                .appName("PixuCord")
+                .completer(new ConsoleCommandCompleter()));
+    }
+
+    @Override
+    protected boolean isRunning() {
+        return MinecraftServer.isStarted() && !MinecraftServer.isStopping();
+    }
+
+    @Override
+    protected void runCommand(String command) {
+        commandManager.execute(commandManager.getConsoleSender(), command);
+    }
+
+    @Override
+    protected void shutdown() {
+        MinecraftServer.LOGGER.info("Shutting down server...");
+        try {
+            MinecraftServer.stopCleanly();
+            System.exit(0);
+        } catch (Throwable t) {
+            System.exit(1);
+        }
+    }
+}
+
